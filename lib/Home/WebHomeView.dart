9ede9722d05ebe156baf1b310be7ac2f0669c0ee
Dchidre <dam.david.chircea@gmail.com>
2024-06-11 02:25:15 +0200

    webHomeView adaptado

--- lib/Home/WebHomeView.dart
+++ lib/Home/WebHomeView.dart
@@ -1,85 +1,98 @@
-
-
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:exa_chircea/components/posts/gridPost.dart';
 import 'package:flutter/material.dart';
 
 import '../FbObjects/fbPost.dart';
+import '../FbObjects/fbUser.dart';
 import '../Singletone/DataHolder.dart';
-import '../components/posts/listPost.dart';
 
 class WebHomeView extends StatefulWidget {
   @override
   State<WebHomeView> createState() => _WebHomeViewState();
 }
 
-class _WebHomeViewState extends State<WebHomeView>{
-
-  //var
+class _WebHomeViewState extends State<WebHomeView> {
+  // Variables
   FirebaseFirestore db = FirebaseFirestore.instance;
   final List<fbPost> postList = [];
 
-  //methods
-  @override
-  void downloadPosts() async{
-    CollectionReference<fbPost> ref=db.collection("Posts")
-        .withConverter(fromFirestore: fbPost.fromFirestore,
-      toFirestore: (fbPost post, _) => post.toFirestore(),);
-
-
-    QuerySnapshot<fbPost> querySnapshot=await ref.get();
-    for(int i=0;i<querySnapshot.docs.length;i++){
-      setState(() {
-        postList.add(querySnapshot.docs[i].data());
-      });
-
-    }
-
-  }
-  void onPostTapDo(int index) {
-    DataHolder().selectedPost = postList[index];
-    DataHolder().savePostInCache();
-    Navigator.of(context).pushNamed('/postview');
-  }
-
-  //widgets
-  Widget? gridCreator(BuildContext context, int index){
-    return gridPost(
-        sUserName: postList[index].sUserName,
-        urlImg: postList[index].sUrlImg,
-        iPos: index, //para que el propio post sea consciente de su posición
-        onPostTap: onPostTapDo,
-    );
-  }
-  Widget grid() {
-    return GridView.builder(
-      gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
-        crossAxisCount: 3,
-      ),
-      itemCount: postList.length,
-      itemBuilder: gridCreator,
-
-    );
-  }
-
-  //initialize statics
+  // Métodos
   @override
   void initState() {
     super.initState();
     downloadPosts();
   }
 
-  //paint
+  void downloadPosts() async {
+    CollectionReference<fbPost> ref = db.collection("Posts")
+        .withConverter(
+      fromFirestore: fbPost.fromFirestore,
+      toFirestore: (fbPost post, _) => post.toFirestore(),
+    );
+
+    QuerySnapshot<fbPost> querySnapshot = await ref.get();
+    for (int i = 0; i < querySnapshot.docs.length; i++) {
+      setState(() {
+        postList.add(querySnapshot.docs[i].data());
+      });
+    }
+  }
+
+  void onPostTapDo(int index) {
+    DataHolder().selectedPost = postList[index];
+    DataHolder().savePostInCache();
+    Navigator.of(context).pushNamed('/postView');
+  }
+
+  // Widgets
+  Widget? gridCreator(BuildContext context, int index) {
+    final post = postList[index];
+    return FutureBuilder<fbUser>(
+      future: DataHolder().fbAdmin.getUser(post.idUser),
+      builder: (context, snapshot) {
+        if (snapshot.connectionState == ConnectionState.waiting) {
+          return LinearProgressIndicator(color: Colors.black, backgroundColor: Colors.grey);
+        } else if (snapshot.hasError) {
+          return Text('Error fetching user data');
+        } else if (!snapshot.hasData) {
+          return Text('No user data available');
+        } else {
+          final user = snapshot.data!;
+          return GridPost(
+            sUserName: post.sUserName,
+            sAvatar: user.sAvatar,
+            urlImg: post.sUrlImg,
+            iPos: index,
+            onPostTap: onPostTapDo,
+          );
+        }
+      },
+    );
+  }
+
+  Widget grid() {
+    return GridView.builder(
+      gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
+        crossAxisCount: 3, // Número de columnas en la grilla
+        crossAxisSpacing: 10.0,
+        mainAxisSpacing: 10.0,
+        childAspectRatio: 0.75, // Ajustar el aspect ratio para hacer las tarjetas más pequeñas verticalmente
+      ),
+      itemCount: postList.length,
+      itemBuilder: gridCreator,
+    );
+  }
+
+  // Pintar
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       body: Center(
-        child:
-          Padding(padding: EdgeInsets.symmetric(horizontal: DataHolder().platformAdmin.getScreenWidth()*0.15),
-            child:
-              grid(),
-          ),
-      )
+        child: Padding(
+          padding: EdgeInsets.symmetric(horizontal: DataHolder().platformAdmin.getScreenWidth() * 0.15),
+          child: grid(),
+        ),
+      ),
     );
   }
 }
--- lib/components/posts/gridPost.dart
+++ lib/components/posts/gridPost.dart
@@ -1,15 +1,18 @@
-
 import 'package:flutter/material.dart';
+import '../../Singletone/DataHolder.dart';
+import '../customIconBtn.dart';
 
-class gridPost extends StatelessWidget {
+class GridPost extends StatelessWidget {
   final String sUserName;
+  final String sAvatar;
   final int iPos;
   final String urlImg;
   final Function(int indice)? onPostTap;
 
-  const gridPost({
+  const GridPost({
     Key? key,
     required this.sUserName,
+    required this.sAvatar,
     required this.iPos,
     this.urlImg = "",
     required this.onPostTap,
@@ -21,32 +24,55 @@ class gridPost extends StatelessWidget {
       onTap: () {
         onPostTap!(iPos);
       },
-      child: Column(
-        crossAxisAlignment: CrossAxisAlignment.start,
-        children: [
-          Padding(
-            padding: const EdgeInsets.all(8.0),
-            child: Row(
-              children: [
-                Container(
-                  width: 20,
-                  height: 20,
-                  decoration: BoxDecoration(
-                    shape: BoxShape.circle,
-                    color: Colors.grey,
-                  ),
-                ),
-                SizedBox(width: 8),
-                Text(sUserName),
-              ],
+      child: Container(
+        margin: EdgeInsets.all(10.0),
+        padding: EdgeInsets.all(10.0),
+        decoration: BoxDecoration(
+          borderRadius: BorderRadius.circular(10.0),
+          color: Colors.white,
+          boxShadow: [
+            BoxShadow(
+              color: Colors.black.withOpacity(0.5),
+              spreadRadius: 0,
+              blurRadius: 10,
+              offset: Offset(0, 2),
             ),
-          ),
-          SizedBox(height: 8),
-          Expanded(
-            child: Padding(
-              padding: const EdgeInsets.symmetric(horizontal: 8.0),
+          ],
+        ),
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.start,
+          crossAxisAlignment: CrossAxisAlignment.stretch,
+          children: [
+            Padding(
+              padding: EdgeInsets.all(10.0),
+              child: Row(
+                children: [
+                  Container(
+                    width: 40.0,
+                    height: 40.0,
+                    decoration: BoxDecoration(
+                      shape: BoxShape.circle,
+                      image: DecorationImage(
+                        image: NetworkImage(sAvatar),
+                        fit: BoxFit.cover,
+                      ),
+                    ),
+                  ),
+                  SizedBox(width: 10.0),
+                  Expanded(
+                    child: Text(
+                      sUserName,
+                      overflow: TextOverflow.ellipsis,
+                    ),
+                  ),
+                ],
+              ),
+            ),
+            Expanded(
               child: Container(
                 decoration: BoxDecoration(
+                  borderRadius: BorderRadius.circular(10.0),
+                  color: Colors.white,
                   image: DecorationImage(
                     image: NetworkImage(urlImg),
                     fit: BoxFit.cover,
@@ -54,8 +80,16 @@ class gridPost extends StatelessWidget {
                 ),
               ),
             ),
-          ),
-        ],
+            SizedBox(height: 10.0), // Espacio entre la imagen y los botones
+            Row(
+              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+              children: [
+                customIconBtn(icono: Icon(Icons.edit, color: Colors.white), fAction: () {}),
+                customIconBtn(icono: Icon(Icons.chat, color: Colors.white), fAction: () {}),
+              ],
+            ),
+          ],
+        ),
       ),
     );
   }
